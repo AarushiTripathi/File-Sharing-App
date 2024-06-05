d2d8e791443b2ac793a8977f4906311cb33edf55
Aarushi Tripathi <137646575+AarushiTripathi@users.noreply.github.com>
2024-06-05 11:39:54 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const File = require('../models/file');
 
 
+// when someone has link, the moment they click this link it will search from database and download the file
 router.get('/:uuid', async (req, res) =>{
     const file = await File.findOne({uuid: req.params.uuid});
     if(!file){
